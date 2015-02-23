b63c290a1e388d5218b560c7d70387b2230d6f97
Cem Olcay <ccemolcay@gmail.com>
2015-02-23 15:21:32 +0200

    without storyboard

--- DroppyMenuController/AppDelegate.swift
+++ DroppyMenuController/AppDelegate.swift
@@ -8,39 +8,66 @@
 
 import UIKit
 
+class BlockButton: UIButton {
+    
+    init (x: CGFloat, y: CGFloat, w: CGFloat, h: CGFloat) {
+        super.init (frame: CGRect (x: x, y: y, width: w, height: h))
+    }
+    
+    override init (frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    var actionBlock: ((sender: BlockButton) -> ())? {
+        didSet {
+            self.addTarget(self, action: "action:", forControlEvents: UIControlEvents.TouchUpInside)
+        }
+    }
+    
+    func action (sender: BlockButton) {
+        actionBlock! (sender: sender)
+    }
+}
+
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    var menuController: DroppyMenuViewController!
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+
+        let first = getViewController(UIColor.blueColor(), title: "First")
+        let second = getViewController(UIColor.redColor(), title: "Second")
+        let third = getViewController(UIColor.yellowColor(), title: "Third")
+        
+        menuController = DroppyMenuViewController (viewControllers: [first, second, third])
+        
+        window = UIWindow (frame: UIScreen.mainScreen().bounds)
+        window!.rootViewController = menuController
+        window!.makeKeyAndVisible()
+        
         return true
     }
 
-    func applicationWillResignActive(application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    func getViewController (color: UIColor, title: String) -> UIViewController {
+        let vc = UIViewController ()
+        vc.view.backgroundColor = color
+        vc.title = title
+        
+        let button = BlockButton (frame: CGRect (x: 10, y: 30, width: 150, height: 60))
+        button.setTitle("menu", forState: .Normal)
+        button.actionBlock = { sender in
+            self.menuController.openMenu()
+        }
+        vc.view.addSubview(button)
+        
+        return vc
     }
-
-    func applicationDidEnterBackground(application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
-
-    func applicationWillEnterForeground(application: UIApplication) {
-        // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-    }
-
-    func applicationDidBecomeActive(application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
-    func applicationWillTerminate(application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
-
-
 }
 
